17b4532bed96aa5de66234a5ea94f1b1c72b9ae2
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-14 20:00:31 -0300

    representative controller refactor and fix

---
+++ controllers/representative/representative_create_controller.py
@@ -0,0 +1,13 @@
+from database import database;
+from models.representative_model import Representative;
+
+# controller é tudo q mexe com banco, cutuca o banco
+def register_representative_controller(data):
+    newrepresentative = Representative(
+       name=data['name'],
+       cnpj=data['cnpj'],
+       email=data['email'],
+       phone=data['phone'],
+    );
+    database.session.add(newrepresentative);
+    database.session.commit();
