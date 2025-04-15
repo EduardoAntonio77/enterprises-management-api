4f96621a9f91f84bc50870489f548bc6c2ddc49d
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-15 17:47:02 -0300

    Created a controller for enterprise registration

---
+++ controllers/enterprise/enterprise_create_controller.py
@@ -0,0 +1,15 @@
+# Imports
+from database import database
+from models.enterprise_model import Enterprise
+
+def create_enterprise(data):
+    new_enterprise = Enterprise(
+        name=data['name'],
+        client_quantity=data["lient_quantity"],
+        address=data['address'],
+        phone=data['phone'],
+        representative_id=["representative_id"]
+    )
+
+    database.session.add(new_enterprise)
+    database.session.commit()
