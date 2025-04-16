36a9c9d2c56e4710f0798665f37cc67ea11789b1
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-16 19:05:43 -0300

    fix(representative_delete_controller.py): removed duplicate function

--- controllers/representative/representative_delete_controller.py
+++ controllers/representative/representative_delete_controller.py
@@ -5,8 +5,7 @@ from datetime import timezone, datetime;
 def delete_representative_controller(repre_id):
     repre = Representative.query.get(repre_id);
 
-    repre.delete_at = datetime.now(timezone.utc); # Caso não aconteça nenhum dos erros acima, preenchera o campo "deleted_at" do id passado com o horario atual.
-    
-    repre.deleted_at = datetime.now(timezone.utc);
+    repre.delete_at = datetime.now(timezone.utc);
+
     database.session.commit();
     return repre;
