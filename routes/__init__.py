a2a79711d8ff9cacc20570b769dfcc9dc5428548
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-15 21:30:53 -0300

    chore(.gitignore): Created a .gitignore

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,15 +2,17 @@ def register_routes(app):
     from .slash_route import slash_blueprint;
     from .representative.representative_create_route import create_representant_blueprint;
     from .representative.representative_delete_route import delete_representative_blueprint
+    from .enterprise.enterprise_create_route import create_enterprise_blueprint
 
     # slash
     app.register_blueprint(slash_blueprint);
 
     # representant
-    app.register_blueprint(create_representant_blueprint)
-    app.register_blueprint(delete_representative_blueprint)
+    app.register_blueprint(create_representant_blueprint);
+    app.register_blueprint(delete_representative_blueprint);
 
     # enterprise
+    app.register_blueprint(create_enterprise_blueprint)
 
     # client
 
