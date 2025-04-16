99dbcdade5370f7c7be983a7193cc633a0ed790a
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-16 17:33:01 -0300

    feat(enterprise_delete_controller): Created a controller for the enterprise delete function

---
+++ controllers/enterprise/enterprise_delete_controller.py
@@ -0,0 +1,11 @@
+from database import database;
+from models.enterprise_model import Enterprise;
+from datetime import datetime, timezone;
+
+def enterprise_delete_controller(id):
+    enterprise = Enterprise.query.get(id);
+
+    enterprise.deleted_at = datetime.now(timezone.utc);
+
+    database.session.commit();
+    return enterprise;
