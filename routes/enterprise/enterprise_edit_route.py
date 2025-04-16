2e840bee4d112193b2ce7a167297355bb1472b21
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-16 16:47:30 -0300

    feat(enterprise_edit_route.py): Created a route for the enterprise edit function

---
+++ routes/enterprise/enterprise_edit_route.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, Blueprint
+
+from middlewares.enterprise.enterprise_edit_middleware import enterprise_edit_middleware
+from controllers.enterprise.enterprise_edit_controller import enterprise_edit_controller
+
+enterprise_edit_blueprint = Blueprint('enterprise_edit_route', __name__)
+
+@enterprise_edit_blueprint.route("/enterprise/<int:id>", methods=['PUT'])  # ✅ CERTO
+
+def edit_enterprise(id):
+    enterprise = enterprise_edit_middleware(id)
+    if not enterprise:
+        return jsonify({
+            'status': 404,
+            'error': 'Enterprise not found'
+        }), 404
+    
+    data = request.get_json()
+    enterprise_edit_controller(data, enterprise)
+
+    return jsonify({
+        'status': 200,
+        'message': 'Enterprise updated successfully'
+    }), 200
