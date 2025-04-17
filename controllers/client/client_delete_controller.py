a8fcf1f8a9301e345211fb667b32c6f31b92e666
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:21:53 -0300

    feat(client_delete_controller.py): Created a function for client remove

---
+++ controllers/client/client_delete_controller.py
@@ -0,0 +1,22 @@
+from flask import jsonify
+from models.client_model import Client
+from database import database
+from datetime import datetime, timezone
+from middlewares.client.client_delete_middleware import validate_client_exists
+
+def client_delete_controller(id):
+    error = validate_client_exists(id)
+    if error:
+        return jsonify({
+            'error': error
+            }), 404
+
+    client = Client.query.get(id)
+    client.deleted_at = datetime.now(timezone.utc)
+
+    database.session.commit()
+
+    return jsonify({
+        'status': 200,
+        'message': f'Client {id} deleted successfully'
+        }), 200
