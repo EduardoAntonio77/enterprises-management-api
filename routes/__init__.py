03cac3f12257047350a259c7af9afaae7a401083
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 19:21:14 -0300

    feat(__init__.py): registered client, delete and edit client routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -12,6 +12,17 @@ def register_routes(app):
     from .enterprise.enterprise_edit_route import enterprise_edit_blueprint;
     from .enterprise.enterprise_delete_route import enterprise_delete_blueprint
 
+    # Client_route
+    from .client.client_create_route import client_create_blueprint
+    from .client.client_delete_route import client_delete_blueprint
+    from .client.client_edit_route import client_edit_blueprint
+
+    # Product_route
+    from .product.product_create_route import product_create_blueprint
+    from .product.product_delete_route import product_delete_blueprint
+    from .product.product_edit_route import product_edit_blueprint
+
+
     # slash
     app.register_blueprint(slash_blueprint);
 
@@ -28,5 +39,11 @@ def register_routes(app):
     
 
     # client
+    app.register_blueprint(client_create_blueprint)
+    app.register_blueprint(client_delete_blueprint)
+    app.register_blueprint(client_edit_blueprint)
 
     # product
+    app.register_blueprint(product_create_blueprint)
+    app.register_blueprint(product_delete_blueprint)
+    app.register_blueprint(product_edit_blueprint)
