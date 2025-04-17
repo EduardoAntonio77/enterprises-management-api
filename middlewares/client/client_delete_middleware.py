6b83e79c3e0d6280057a6eec67f82bc4ba8e6afe
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:33:59 -0300

    feat(client_delete_middleware.py): Create a middleware for client removal

---
+++ middlewares/client/client_delete_middleware.py
@@ -0,0 +1,14 @@
+from models.client_model import Client
+
+def validate_client_exists(id):
+    client = Client.query.get(id)
+    if not client or client.deleted_at:
+        return "Client not found or already deleted"
+    
+
+    return 'Client deleted successfully'
+    
+
+    return None
+
+    
