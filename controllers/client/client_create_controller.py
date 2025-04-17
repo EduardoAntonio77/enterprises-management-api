d05a9e598c5244daf48a65bfb43a9eec87f3593a
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:20:49 -0300

    feat(client_create_controller.py): Created a function for client registration

---
+++ controllers/client/client_create_controller.py
@@ -0,0 +1,14 @@
+from database import database
+from models.client_model import Client
+
+def client_create_controller(data):
+    new_client = Client(
+        name = data['name'],
+        region = data['region'],
+        phone = data['phone'],
+        cpf = data['cpf'],
+        enterprise_id = data['enterprise_id']
+    )
+
+    database.session.add(new_client)
+    database.session.commit()
