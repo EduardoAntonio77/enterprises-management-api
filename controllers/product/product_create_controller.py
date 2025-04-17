2eb8389d6d5ee1eb58dbaa4cfe4ce5785c93a2aa
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:28:07 -0300

    feat(product_create_controller.py): Created a function for product registration

---
+++ controllers/product/product_create_controller.py
@@ -0,0 +1,19 @@
+from database import database
+from models.product_model import Product
+
+def product_create_controller(data):
+    required_fields = ['name', 'price', 'stock', 'enterprise_id']
+    for field in required_fields:
+        if field not in data:
+            return f"Missing field: {field}"
+
+        new_product = Product(
+            name=data['name'],
+            price=float(data['price']),  
+            stock=int(data['stock']),
+            enterprise_id=int(data['enterprise_id'])
+        )
+
+    database.session.add(new_product)
+    database.session.commit()
+    return None
