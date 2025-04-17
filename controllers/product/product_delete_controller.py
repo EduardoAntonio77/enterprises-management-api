c4f174aae36fbf7528139046cd026023d6f62809
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:28:52 -0300

    feat(product_delete_controller.py): Created a function for delete product

---
+++ controllers/product/product_delete_controller.py
@@ -0,0 +1,8 @@
+from database import database
+from models.product_model import Product
+
+def product_delete_controller(id):
+    product = Product.query.filter_by(id=id).first()
+    
+    database.session.delete(product)
+    database.session.commit()
