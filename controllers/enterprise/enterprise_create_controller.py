3b3da23b84ff6c7a68d002d992143c3fa1d232ff
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-17 17:26:30 -0300

    chore(enterprise_create_controller.py): rename parameter enterprise_id

--- controllers/enterprise/enterprise_create_controller.py
+++ controllers/enterprise/enterprise_create_controller.py
@@ -8,7 +8,7 @@ def create_enterprise(data):
         client_quantity=data["client_quantity"],
         address=data['address'],
         phone=data['phone'],
-        representative_id=data['representative_id']
+        enterprise_id=data['enterprise_id']
     )
 
     database.session.add(new_enterprise)
