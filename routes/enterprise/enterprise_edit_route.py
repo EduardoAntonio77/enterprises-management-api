d02d73a27a48dacdd9cb676041bfbe1e07879f49
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-18 22:38:57 -0300

    refactor(enterprise_edit_route.py): remove unnecessary imports

--- routes/enterprise/enterprise_edit_route.py
+++ routes/enterprise/enterprise_edit_route.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint
 
 from middlewares.enterprise.enterprise_edit_middleware import enterprise_edit_middleware
 from controllers.enterprise.enterprise_edit_controller import enterprise_edit_controller
 
 enterprise_edit_blueprint = Blueprint('enterprise_edit_route', __name__)
 
-@enterprise_edit_blueprint.route("/enterprise/<int:id>", methods=['PUT'])  # ✅ CERTO
+@enterprise_edit_blueprint.route("/enterprise/<int:id>", methods=['PUT'])
 
 def edit_enterprise(id):
     enterprise = enterprise_edit_middleware(id)
