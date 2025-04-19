e11ec2bfd092cfda0c0662345867ca10e0acabf5
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-18 22:30:03 -0300

    feat(product_create_route.py): Create a route for product registion

---
+++ routes/product/product_create_route.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+
+from middlewares.product.product_create_middleware import product_create
+from controllers.product.product_create_controller import product_create_controller
+
+product_create_blueprint = Blueprint('create_product_blueprint', __name__)
+@product_create_blueprint.route("/product", methods=['POST'])
+def register_product():
+    data = request.json;
+
+    error = product_create(data)
+    if error:
+        return jsonify({
+            'status': 400,
+            'error': error
+        }), 400;
+
+    product_create_controller(data)
+
+    return jsonify({
+        "status": 200,
+        'message': 'Successfully registered product'
+    })
