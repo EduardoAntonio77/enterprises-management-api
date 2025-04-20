d5a08087d645649f6e8ab2559296ce66406c6b7c
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-19 21:39:47 -0300

    fix(client_model.py): Changing the primitive type of a column(cpf)

--- models/client_model.py
+++ models/client_model.py
@@ -9,7 +9,7 @@ class Client(database.Model):
     name = database.Column(database.String(200), nullable = False);
     region = database.Column(database.String(100), nullable = False);
     phone = database.Column(database.String(50), nullable = False, unique = True);
-    cpf = database.Column(database.Float, nullable = False, unique = True);
+    cpf = database.Column(database.String(50), nullable = False, unique = True);
     created_at = database.Column(database.DateTime, nullable = False, default = datetime.now(timezone.utc));
     deleted_at = database.Column(database.DateTime, nullable = True);
 
