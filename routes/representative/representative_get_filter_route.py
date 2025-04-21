b81b82ce43a9e14e25b347d56e4cfe5adc4d7be2
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-21 16:46:55 -0300

    refactor(representative_get_filter_route.py): refactor for better modularization

--- routes/representative/representative_get_filter_route.py
+++ routes/representative/representative_get_filter_route.py
@@ -1,18 +1,13 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, g
 from middlewares.representative.representative_get_filter_middleware import representative_search
 from flask_jwt_extended import jwt_required
+from controllers.representative.representative_get_filter_controller import representative_filter
 
 representative_get_filter_blueprint = Blueprint('representative_get_filter_route', __name__)
 
 @representative_get_filter_blueprint.route("/representative/<int:id>", methods=["GET"])
-@representative_search  # Aplica o middleware antes da rota
+@representative_search
 @jwt_required()
-def representative_filter(representative, id):  # O 'representative' é passado pelo middleware
-    return jsonify({
-        "id": representative.id,
-        "email": representative.email,
-        "name": representative.name,
-        'cnpj': representative.cnpj,
-        "phone": representative.phone,
-        "created_at": representative.created_at.isoformat()
-    }), 200
+def representative_get_filter(id, representative):
+    return representative_filter(representative=representative, id=id)
+
