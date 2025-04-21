f56ba3c73a37b9e5ad5758c2e71d232deb441678
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-21 16:47:52 -0300

    refactor(representative_edit_route.py): refactor for better modularization

--- routes/representative/representative_edit_route.py
+++ routes/representative/representative_edit_route.py
@@ -1,6 +1,7 @@
-from flask import jsonify, Blueprint
+from flask import request, Blueprint, jsonify
 from middlewares.representative.representative_edit_middleware import representative_middleware
 from flask_jwt_extended import jwt_required
+from controllers.representative.representative_edit_controller import edit_representative_controller
 
 edit_representative_blueprint = Blueprint('edit_representative_route', __name__) # Criando um blueprint para a rota.
 
@@ -9,8 +10,13 @@ edit_representative_blueprint = Blueprint('edit_representative_route', __name__)
 def edit_representative(id): # Criando uma função que sera executada aoacessar a rota.
 
     repre = representative_middleware(id) # Criando uma variavel que recebera um chamado para a função importada no inicio do código.
+
+    data = request.json
+
     if not repre:
         return jsonify({
             "status": 404,
             "error": "Representative not found"
         }), 404 # Caso o o representante com o id informado não exista, retornara uma respota json informando um erro 404 (not found)
+    
+    return edit_representative_controller(repre, data)
