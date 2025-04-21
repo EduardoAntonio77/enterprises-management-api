e2fa250403636c91d0b2cf6f7ba87a3c2c74d90f
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-21 16:48:42 -0300

    refactor(representative_edit_middleware.py): refactor for better modularization

--- middlewares/representative/representative_edit_middleware.py
+++ middlewares/representative/representative_edit_middleware.py
@@ -1,4 +1,4 @@
 from models.representative_model import Representative # Imports
 
 def representative_middleware(id): # Criando uma função que recebera como parametro um id vindo de sua respectiva rota
-    return Representative.query.id(id) # Ira fazer uma busca no banco de dados na tabela de representante pelo id passado na requisição da rota.
+    return Representative.query.id(id) # Irá fazer uma busca no banco de dados na tabela de representante pelo id passado na requisição da rota.
