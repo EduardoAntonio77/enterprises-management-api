24f9f3f1e2b1edb150679a4c5bd5155a4fb05295
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-27 16:39:36 -0300

    feat(enterprise_get_route.py): Create a route for enterprise view fuction

---
+++ api/routes/enterprise/enterprise_get_route.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
+from controllers.enterprise.enterprise_get_controller import get_enterprises
+
+enterprise_get_blueprint = Blueprint('enterprise_get_route', __name__)
+
+@enterprise_get_blueprint.route('/enterprise', methods=['GET'])
+@jwt_required
+def enterprise_get():
+    return jsonify(get_enterprises()), 200
