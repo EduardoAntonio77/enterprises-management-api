ec76f42b468997009878042fa76ff2c22bb8bea9
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-27 19:22:24 -0300

    feat(product_get_route.py): Create a route for view products list

---
+++ api/routes/product/product_get_route.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
+from api.controllers.product.product_get_controller import get_products
+
+product_get_blueprint = Blueprint('product_get_route', __name__)
+
+@product_get_blueprint.route("/product", methods=["GET"])
+@jwt_required()
+def product_get():
+    return jsonify(get_products()), 200
