61837926fff53cad6930fe1be59f3581cce7b56b
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-27 16:51:02 -0300

    fix(enterprise_get_route.py): Fix jwt_required fuction()

--- api/routes/enterprise/enterprise_get_route.py
+++ api/routes/enterprise/enterprise_get_route.py
@@ -5,6 +5,6 @@ from controllers.enterprise.enterprise_get_controller import get_enterprises
 enterprise_get_blueprint = Blueprint('enterprise_get_route', __name__)
 
 @enterprise_get_blueprint.route('/enterprise', methods=['GET'])
-@jwt_required
+@jwt_required()
 def enterprise_get():
     return jsonify(get_enterprises()), 200
