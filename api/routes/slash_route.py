558ed71bdf2344488fc45bb69452478a46772d9b
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-28 21:15:50 -0300

    feat(app.py): Create and config fuction Swagger and Flasgger for document routes

--- api/routes/slash_route.py
+++ api/routes/slash_route.py
@@ -1,13 +1,32 @@
-# imports
-from flask import Blueprint, jsonify;
+from flask import Blueprint, jsonify
+from flasgger import swag_from
 
 slash_blueprint = Blueprint('slash_blueprint', __name__)
 
-# slash route
+# Slash route
 @slash_blueprint.route('/')
+@swag_from({
+    'tags': ['Home page'],
+    'description': 'Endpoint raiz que retorna informações sobre o banco de dados de empresas.',
+    'responses': {
+        200: {
+            'description': 'Informações do banco de dados de empresas retornadas com sucesso!',
+            'examples': {
+                'application/json': {
+                    'status': 200,
+                    'message': 'Enterprises Database',
+                    'version': 0.1
+                }
+            }
+        }
+    }
+})
 def slashroute():
+    """
+    Endpoint raiz que retorna informações sobre o banco de dados de empresas
+    """
     return jsonify({
         'status': 200,
         'message': 'Enterprises Database',
         'version': 0.1
-    });
+    })
