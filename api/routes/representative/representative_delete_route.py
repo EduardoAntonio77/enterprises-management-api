1053f10c076ff87d3e9c096450da6430c5830eca
Eduardo Antônio <fantauva250ml@gmail.com>
2025-04-28 21:20:54 -0300

    feat(representative_delete_route.py): Increment documentation route using flasgger

--- api/routes/representative/representative_delete_route.py
+++ api/routes/representative/representative_delete_route.py
@@ -1,32 +1,64 @@
-# imports
-from flask import jsonify, Blueprint;
+from flask import jsonify, Blueprint
 from flask_jwt_extended import jwt_required
+from flasgger import swag_from
 
-# middlewares & controllers
-from middlewares.representative.representative_delete_middleware import representative_delete_middleware;
-from controllers.representative.representative_delete_controller import delete_representative_controller;
+from middlewares.representative.representative_delete_middleware import representative_delete_middleware
+from controllers.representative.representative_delete_controller import delete_representative_controller
 
-# Criando um blueprint para a rota.
-delete_representative_blueprint = Blueprint("representative_routes", __name__); 
+delete_representative_blueprint = Blueprint("representative_routes", __name__)
 
-# Criando uma rota que utilizara apenas o método DELETE, ela ira converter o trecho "<int:id>" da URL para um int e manda como argumento para a função usa-la.
-@delete_representative_blueprint.route('/representative/<int:id>', methods=['DELETE']) 
+@delete_representative_blueprint.route('/representative/<int:id>', methods=['DELETE'])
 @jwt_required()
+@swag_from({
+    'tags': ['Representative'],
+    'description': 'Endpoint para excluir um representante pelo ID.',
+    'parameters': [
+        {
+            'name': 'id',
+            'in': 'path',
+            'required': True,
+            'type': 'integer',
+            'description': 'ID do representante a ser excluído',
+            'example': 1
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Representante excluído com sucesso!',
+            'examples': {
+                'application/json': {
+                    'status': 200,
+                    'message': 'Representative successfully deleted!'
+                }
+            }
+        },
+        400: {
+            'description': 'Erro ao tentar excluir o representante.',
+            'examples': {
+                'application/json': {
+                    'status': 400,
+                    'message': 'Erro: Representante não encontrado.'
+                }
+            }
+        }
+    }
+})
 def delete_representative(id):
-    # chamando middleware
+    """
+    Endpoint para excluir um representante.
+    """
+
     error = representative_delete_middleware(id)
-    # se o middleware retornar qualquer coisa != de None...
+
     if error:
         return jsonify({
             "status": 400,
             "message": error,
-        }), 400;
+        }), 400
 
-    # se o middleware não retornar nada (ou seja: não retornar erro), chamar o controller
     delete_representative_controller(id)
+
     return jsonify({
         'status': 200,
         'message': 'Representative successfully deleted!'
-    }), 200;
-
-
+    }), 200
