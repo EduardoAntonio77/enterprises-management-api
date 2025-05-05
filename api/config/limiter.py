e36f0348cb301023104b9a58741c6e8198648f02
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-05 18:25:46 -0300

    feat(limiter.py): Create a config file for flask limiter configs

---
+++ api/config/limiter.py
@@ -0,0 +1,11 @@
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from redis import Redis
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["10 per minute"]
+)
+
+def init_limiter(app):
+    limiter.init_app(app)
