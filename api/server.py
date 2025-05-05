650fc5aaea3ea34ad1a78ae539b93b80ca973c06
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-05 18:23:25 -0300

    feat(server.py): increase https licenses to the application

--- api/server.py
+++ api/server.py
@@ -14,9 +14,8 @@ flask_app = app_instance.app
 flask_app.config['DEBUG'] = True
 port_dotenv = os.environ.get("PORT")
 
-# Para rodar localmente com `python server.py`
 if __name__ == "__main__":    
     print(f"API running in: http://127.0.0.1:{port_dotenv}")
     print("API Documentation running in: http://localhost:5000/apidocs")
-    # serve(flask_app, host="0.0.0.0", port=port_dotenv)
-    flask_app.run(ssl_context='adhoc', debug=True)
+    # flask_app.run(ssl_context='adhoc', debug=True)
+    flask_app.run(host="127.0.0.1", port=5000, debug=True)
