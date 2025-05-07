70099f8730db23cdaa222470865ba7844e016135
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-07 18:58:24 -0300

    feat(product_filters_model.py): Create a new model

---
+++ api/models/product_filters_model.py
@@ -0,0 +1,11 @@
+from config.database import database
+from datetime import datetime, timezone
+
+class ProductFilters(database.Model):
+    __tablename__ = 'product_filters'
+    filter_id = database.Column(database.Integer, primary_key = True, autoincrement = True)
+    filter_name = database.Column(database.String(100), nullable = False)
+    created_at = database.Column(database.DateTime, default = datetime.now(timezone.utc), nullable = False)
+    deleted_at = database.Column(database.DateTime, nullable = True)
+
+    product_id = database.Column(database.Integer, database.ForeignKey('product.id'), nullable = False);
