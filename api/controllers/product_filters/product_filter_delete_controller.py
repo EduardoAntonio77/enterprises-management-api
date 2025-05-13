9a93a643ca40ff82a0677471981fb719d041718a
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-13 17:12:56 -0300

    feat(product_filter_delete_middleware.py): Rename query filter in fuction

--- api/controllers/product_filters/product_filter_delete_controller.py
+++ api/controllers/product_filters/product_filter_delete_controller.py
@@ -1,8 +1,8 @@
 from config.database import database
 from models.product_filters_model import ProductFilters
 
-def product_filter_delete_controller(id):
-    product_filter = ProductFilters.query.filter_by(id=id).first()
+def product_filter_delete_controller(filter_id):
+    product_filter = ProductFilters.query.filter_by(filter_id=filter_id).first()
 
     database.session.delete(product_filter)
     database.session.commit()
