127ca3925d6a4ae39acdf2023fffbe44db7beeb6
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-16 17:05:48 -0300

    feat(product_by_filter_get_route.py): Create a documentation using flasger for product by filter get route.

--- api/routes/product/product_by_filter_get_route.py
+++ api/routes/product/product_by_filter_get_route.py
@@ -1,11 +1,15 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required
+
 from controllers.product.product_by_filter_get_controller import get_products_by_filters_controller
 from middlewares.product.product_by_filter_get_middleware import get_products_by_filters_middleware
-from flask_jwt_extended import jwt_required
+
+from flasgger.utils import swag_from
 
 get_products_by_filters_blueprint = Blueprint('get_products_by_filters', __name__)
 
 @get_products_by_filters_blueprint.route('/product/filters', methods=['GET'])
+@swag_from("../../../docs/product_docs/product_by_filter_get_docs.yaml")
 @get_products_by_filters_middleware
 @jwt_required()
 def handle_get_products_by_filters():
