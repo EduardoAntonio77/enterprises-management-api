e85c8443261b29e03ff26049aea7921f21343301
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-16 17:07:38 -0300

    feat(product_filter_create_route.py): Create a documentation usgin flassger for product filter create route.

--- api/routes/product_filters/product_filter_create_route.py
+++ api/routes/product_filters/product_filter_create_route.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 
+from flasgger.utils import swag_from
+
 from middlewares.product_filters.product_filter_create_middleware import product_filters_create
 from controllers.product_filters.product_filter_create_controller import create_filter
 
@@ -8,6 +10,7 @@ product_filter_create_blueprint = Blueprint('create_product_filter_blueprint', _
 
 @product_filter_create_blueprint.route("/product_filter", methods=['POST'])
 @jwt_required()
+@swag_from('../../../docs/product_filter_docs/product_filter_create_docs.yaml')
 def register_product():
 
     data = request.json
