4c2331940ed3b407e134d9d9b26fa6b99134ddd0
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-20 21:41:01 -0300

    refactor(product_get_controller.py): Change logic controller

--- api/controllers/product/product_get_controller.py
+++ api/controllers/product/product_get_controller.py
@@ -1,12 +1,29 @@
 from models.product_model import Product
 
 def get_products():
-    products = Product.query.filter(Product.deleted_at.is_(None)).all()
-    return [
-        {
-        'id': prod.id,
-        'name': prod.name,
-        'enterprise_id': prod.enterprise_id
 
-        } for prod in products
-    ]
+    response = []
+
+    for prod in Product.query.filter(Product.deleted_at.is_(None)).all():
+
+        product_filters_data = [{
+            'filter_id': fil.filter_id,
+            'filter_name': fil.filter_name
+        }
+        for fil in prod.filters
+        ]
+
+        products_data = {
+            'product_id': prod.id,
+            'product_name': prod.name,
+            'product_price': prod.price,
+            'product_stock': prod.stock,
+            'enterprise_id': prod.enterprise_id,
+            'filters': [
+                product_filters_data
+            ]
+        }
+        
+        response.append(products_data)
+
+    return response
