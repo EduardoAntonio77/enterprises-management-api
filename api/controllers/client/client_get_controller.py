34d731b22e397de7d5fbffdcd4d207bf13518c40
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-20 21:38:42 -0300

    refactor(client_get_controller.py): Change logic controller

--- api/controllers/client/client_get_controller.py
+++ api/controllers/client/client_get_controller.py
@@ -1,18 +1,18 @@
 from models.client_model import Client
 
 def get_clients():
-    clients = Client.query.all()
+    clients = Client.query.filter(Client.deleted_at.is_(None)).all()
     response = []
 
     for client in clients:
-        client_data = ([
-            ('client_id', client.id),
-            ('client_name', client.name),
-            ('client_region', client.region),
-            ('client_phone', client.phone),
-            ('client_cpf', client.phone),
-            ('enterprise_id', client.enterprise_id)
-        ])
+        client_data = {
+            'client_id': client.id,
+            'client_name': client.name,
+            'client_region': client.region,
+            'client_phone': client.phone,
+            'client_cpf': client.phone,
+            'enterprise_id': client.enterprise_id
+        }
         response.append(client_data)
 
     return response
