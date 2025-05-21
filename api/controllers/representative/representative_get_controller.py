4cb76719872eeb61854af8dcd0597e673aba92a4
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-20 21:43:05 -0300

    refactor(product_get_controller.py): Change logic controller

--- api/controllers/representative/representative_get_controller.py
+++ api/controllers/representative/representative_get_controller.py
@@ -1,56 +1,54 @@
 from models.representative_model import Representative
-from collections import OrderedDict  # opcional
 
 def get_representative():
-    representatives = Representative.query.all()
     response = []
 
-    for rep in representatives:
+    for rep in Representative.query.all():
         if not rep.enterprises:
             continue
 
-        enterprise = rep.enterprises[0]  # se usar apenas a primeira
-
-        # Clients
-        clients_data = {}
-        for idx, client in enumerate(enterprise.clients, start=1):
-            clients_data[str(idx)] = {
-                "client_name": client.name,
-                "client_region": client.region,
-                "client_phone": client.phone,
-                "client_cpf": client.cpf
-            }
-
-        # Products
-        products_data = {}
-        for idx, product in enumerate(enterprise.products, start=1):
-            filters_data = {}
-            for jdx, f in enumerate(product.filters, start=1):
-                filters_data[str(jdx)] = {
-                    "filter_id": f.filter_id,
-                    "filter_name": f.filter_name
+        enterprises_data = []
+        for enterprise in rep.enterprises:
+            clients = [
+                {
+                    "client_name": c.name,
+                    "client_region": c.region,
+                    "client_phone": c.phone,
+                    "client_cpf": c.cpf
                 }
+                for c in enterprise.clients
+            ]
 
-            products_data[str(idx)] = {
-                "product_name": product.name,
-                "product_price": product.price,
-                "product_stock": product.stock,
-                "filters": filters_data
-            }
+            products = [
+                {
+                    "product_name": p.name,
+                    "product_price": p.price,
+                    "product_stock": p.stock,
+                    "filters": [
+                        {
+                            "filter_id": f.filter_id,
+                            "filter_name": f.filter_name
+                        } for f in p.filters
+                    ]
+                }
+                for p in enterprise.products
+            ]
 
-        # Representante + empresa
-        rep_data = OrderedDict()  # se quiser garantir ordem
-        rep_data["id"] = rep.id
-        rep_data["name"] = rep.name
-        rep_data["cnpj"] = rep.cnpj
-        rep_data["email"] = rep.email
-        rep_data["enterprise"] = OrderedDict({
-            "enterprise_id": enterprise.id,
-            "enterprise_name": enterprise.name,
-            "enterprise_address": enterprise.address,
-            "clients": clients_data,
-            "products": products_data
-        })
+            enterprises_data.append({
+                "enterprise_id": enterprise.id,
+                "enterprise_name": enterprise.name,
+                "enterprise_address": enterprise.address,
+                "clients": clients,
+                "products": products
+            })
+
+        rep_data = {
+            "id": rep.id,
+            "name": rep.name,
+            "cnpj": rep.cnpj,
+            "email": rep.email,
+            "enterprises": enterprises_data
+        }
 
         response.append(rep_data)
 
