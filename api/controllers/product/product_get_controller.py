c16964ceeb0bda45976f1625de16b6d46e22bafd
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-21 21:31:55 -0300

    refactor(product_get_controller.py): Add pagination.

--- api/controllers/product/product_get_controller.py
+++ api/controllers/product/product_get_controller.py
@@ -1,16 +1,24 @@
 from models.product_model import Product
+from flask import request
+import json
 
 def get_products():
+    page = request.args.get('page', 1, type=int)          
+    per_page = request.args.get('per_page', 3, type=int)  
+
+    pagination = Product.query.filter(Product.deleted_at.is_(None)).order_by(Product.id).paginate(page=page,per_page=per_page,error_out=False)
+
+    products = pagination.items
 
     response = []
 
-    for prod in Product.query.filter(Product.deleted_at.is_(None)).all():
-
-        product_filters_data = [{
-            'filter_id': fil.filter_id,
-            'filter_name': fil.filter_name
-        }
-        for fil in prod.filters
+    for prod in products:
+        product_filters_data = [
+            {
+                'filter_id': fil.filter_id,
+                'filter_name': fil.filter_name
+            }
+            for fil in prod.filters
         ]
 
         products_data = {
@@ -19,11 +27,19 @@ def get_products():
             'product_price': prod.price,
             'product_stock': prod.stock,
             'enterprise_id': prod.enterprise_id,
-            'filters': [
-                product_filters_data
-            ]
+            'filters': product_filters_data
         }
-        
+
         response.append(products_data)
 
-    return response
+    result = {
+        'page': pagination.page,
+        'per_page': pagination.per_page,
+        'total_pages': pagination.pages,
+        'total_items': pagination.total,
+        'data': response
+    }
+
+    print(json.dumps(result, indent=4))
+
+    return result
