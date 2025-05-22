b075260240b121c56a4f5da0e1d3eaa09eb27a4c
Eduardo Antônio <fantauva250ml@gmail.com>
2025-05-21 21:48:22 -0300

    refactor(product_by_filter_get_middleware.py): Change logic.

--- api/middlewares/product/product_by_filter_get_middleware.py
+++ api/middlewares/product/product_by_filter_get_middleware.py
@@ -1,20 +1,22 @@
-from flask import request, jsonify
+from flask import jsonify
+from functools import wraps
 
 def get_products_by_filters_middleware(func):
-    def wrapper():
-        filters = request.args.get('filters')
-        if not filters:
-            return jsonify({
-                'error': 'The "filters" parameter is mandatory'
-                    }), 400
-        
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        filter_ids = kwargs.get('filter_ids')
+
+        if not filter_ids:
+            return jsonify({'error': 'The "filter_ids" parameter is mandatory'}), 400
+
         try:
-            list(map(int, filters.split(',')))  
+            ids = list(map(int, filter_ids.replace('-', ',').split(',')))
         except ValueError:
-            return jsonify({
-                'error': 'These filters must be integers'
-                            }), 400
-        
-        return func()
-    
+            return jsonify({'error': 'The filters must be integers'}), 400
+
+        # Passa os IDs processados para o controller via kwargs
+        kwargs['ids'] = ids
+
+        return func(*args, **kwargs)
+
     return wrapper
